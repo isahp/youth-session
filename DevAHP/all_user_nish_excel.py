2f42c9847f1f0c6cf670f8e7a63135924cfe88e2
devin719 <devintamayo@gmail.com>
2016-10-23 17:48:11 -0400

    Trying, why won't you work

---
+++ DevAHP/all_user_nish_excel.py
@@ -0,0 +1,16 @@
+from openpyxl import load_workbook
+import numpy as np
+from all_user_votes import PairwiseAllUsers, inv_vote
+
+def from_nish_excel(xlsxFile):
+    offset = find_offset()
+    endrow = find_end_row()
+    
+    #im going to start with psuedo code
+    
+    
+def get_mtx_from_row(xlsxFile, sheet, row):
+    
+
+ 
+ def largest_eigen():
